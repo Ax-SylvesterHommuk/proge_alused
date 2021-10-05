9d468f01320362840ca12d0742cc272da6154907
Ax-Sylvester Hommuk <ax-sylvester.hommuk@khk.ee>
2021-10-05 12:58:45 +0300

    Moodle Üls

---
+++ Praks 1/1.4 Trahv.py
@@ -0,0 +1,14 @@
+import math
+
+print("Sisestage oma nimi: ")
+nimi = input()
+
+print("Sisestage lubatud kiirus (km/h): ")
+a = int(input())
+
+print("Sisestage tegelik kiirus (km/h): ")
+b = int(input())
+
+trahv = ... # Siia trahv
+
+print(nimi + ", kiiruse ületamise eest on teie trahv " + " eurot.")
