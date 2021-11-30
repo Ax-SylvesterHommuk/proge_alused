21b9d93ac0bdb01a52394f78168480038642acb6
Ax-Sylvester Hommuk <ax-sylvester.hommuk@khk.ee>
2021-11-30 13:44:58 +0200

    7.2 Päevik

--- Praks 1/7.2 Päevik.py
+++ Praks 1/7.2 Päevik.py
@@ -1,13 +1,12 @@
-import os
-
 from datetime import datetime
 kuupäev_kellaaeg = datetime.today()
 
-file = open("paevik.txt", encoding = "UTF-8")
+file = open("paevik.txt", "a")
 
 file.write(str(kuupäev_kellaaeg))
 file.write("\n")
+file.write(str(input("Sisesta sissekande tekst: ")))
+file.write("\n\n")
 
-file.write(sissekanne = str(input("Sisesta sissekande tekst: ")))
 file.close()
 
